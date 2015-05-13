978b86611a37f2a933fa237308ac777214833f59
rudig <rudig@RudiG.smartcall.local>
2015-05-13 14:43:52 +0200

    allow existing keystore owners to import easier by setting the config file and password

--- SmartApp2/src/main/java/za/co/smartcall/smartload/view/UseExistingKeystoreController.java
+++ SmartApp2/src/main/java/za/co/smartcall/smartload/view/UseExistingKeystoreController.java
@@ -1,16 +1,21 @@
 package za.co.smartcall.smartload.view;
 
+import java.io.File;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import lombok.extern.log4j.Log4j;
 
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
 import org.controlsfx.dialog.Dialogs;
 
 import za.co.smartcall.smartload.DialogFactory;
 import za.co.smartcall.smartload.MainApp;
 import za.co.smartcall.smartload.RefPreferences;
+import za.co.smartcall.wsclient.DealerSetupException;
 import za.co.smartcall.wsclient.implementation.KeyStoreImplementation;
 
 @Log4j
@@ -20,6 +25,9 @@ public class UseExistingKeystoreController {
     
     @FXML
     private TextField dealerTextField;
+    
+    @FXML
+    private TextField passwordTextField;
         
     @FXML
     private Label keyLocationLabel;
@@ -27,6 +35,22 @@ public class UseExistingKeystoreController {
     private Stage dialogStage;
     
     private boolean okClicked = false;
+    
+	public static final String FILE_PROPERTY_TAG = "org.apache.ws.security.crypto.merlin.file";
+	
+	public static final String PASSWORD_PROPERTY_TAG = "org.apache.ws.security.crypto.merlin.keystore.password";
+	
+	public static final String ALIAS_PROPERTY_TAG = "org.apache.ws.security.crypto.merlin.keystore.alias";
+	
+	public static final String keyStoreProperties = "clientKeystore.properties";
+	
+	public static final String FILE_SEPARATOR = System.getProperty("file.separator");
+	
+	private static String APP_HOME = System.getProperty("user.dir");
+	
+	public static final String CONFIG_HOME = APP_HOME+FILE_SEPARATOR+"config"+FILE_SEPARATOR+"keys";
+	
+	public static final String clientKeyStore = "clientKeystore.jks";
 
     /**
      * The constructor.
@@ -81,11 +105,33 @@ public class UseExistingKeystoreController {
     private void linkToExistingKeystore() {
         try {
 			mainApp.setDealerMsisdn(dealerTextField.getText());
+			passwordTextField.getText();
+			updateDealerDetailsInFile(dealerTextField.getText(), passwordTextField.getText());
 			dialogStage.close();
 		} catch (Exception e) {
 			DialogFactory.createDialogException("Use exisiting key pair", "Error setting new key", e);
 		}
     }
+    
+    /**
+	 * Create a file that has all the keystore properties and the user specific details 
+	 */
+	public void updateDealerDetailsInFile(String msisdn,String password) throws DealerSetupException{
+		try {
+			String keystorePropertiesFileLocation = KeyStoreImplementation.CONFIG_HOME + FILE_SEPARATOR + keyStoreProperties;
+			File file = new File(keystorePropertiesFileLocation);
+			PropertiesConfiguration config = new PropertiesConfiguration(file);
+			config.setProperty(PASSWORD_PROPERTY_TAG, password);
+			config.setProperty(ALIAS_PROPERTY_TAG, msisdn);
+			config.setProperty(FILE_PROPERTY_TAG, KeyStoreImplementation.CONFIG_HOME+FILE_SEPARATOR+clientKeyStore);
+			config.save();
+			log.info("Saved keystore properties file as " + keystorePropertiesFileLocation);
+		} catch (ConfigurationException e) {
+			e.printStackTrace();
+			log.error("Error in dealer detail setup",e);
+			throw new DealerSetupException(e.getMessage());
+		}
+	}
     
 
 }	
